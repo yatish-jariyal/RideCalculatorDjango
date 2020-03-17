91618f31e868c02d7c647c1cdaae89f6478bcb0a
yatis <yatish.j.24@gmail.com>
2020-03-17 13:00:09 +0530

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import FuelPrices
 from django.http import HttpResponse
 import requests
+from django.core import serializers
 
 @never_cache
 @csrf_exempt
@@ -86,4 +87,44 @@ def updateCurrentFuelPrices(request):
     response = dict()
     response['data'] = 'ok'
     return HttpResponse(json.dumps(response), content_type='application/json')
+
+@csrf_exempt
+@never_cache
+def fetchFuelPrice(request):
+    req = request.POST
+    city = req['city']
+    fuel_type = req['fuel_type']
+
+    type = 1
+    if fuel_type == "Diesel":
+        type = 2
+    elif fuel_type == "CNG":
+        type = 3
+
+    query_set = FuelPrices.objects.filter(city=city, fuel_type=type)
+    json_data = serializers.serialize('json',query_set)
+    data = json.loads(json_data)
+
+    print('data = ',data)
+    data = data[0]
+    mydata = dict()
+    mydata['id'] = data['pk']
+    fields = data['fields']
+    mydata['city'] = fields['city']
+    mydata['price'] = fields['price']
+    fuel_type = fields['fuel_type']
+    if fuel_type == 1:
+        mydata['fuel_type'] = 'Petrol'
+    elif fuel_type == 2:
+        mydata['fuel_type'] = 'Diesel'
+    else:
+        mydata['fuel_type'] = 'CNG'
+
+    return HttpResponse(json.dumps(mydata), content_type='application/json')
+
+
+
+
+
+
 
