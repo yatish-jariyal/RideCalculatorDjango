22a3f7f211e0f3927d4147e407b7f3a0b45ba2bb
priyanka <ppakhale@asu.edu>
2020-03-17 15:11:26 +0530

    commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,10 +1,14 @@
-import os
-import sys
+"""
+WSGI config for mysite project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
 
-path = os.path.expanduser('~/mysite')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+import os
+
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = get_wsgi_application()
