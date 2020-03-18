413d4472cd63a193b049e4f18a0e2e15bb9049b4
priyanka <ppakhale@asu.edu>
2020-03-18 18:33:00 +0530

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'yatish13$mydb',
-        'USER': 'yatish13',
-        'PASSWORD': 'placybabe',
-        'HOST': 'yatish13.mysql.pythonanywhere-services.com',
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'yatish13$mydb',
+        #'USER': 'yatish13',
+        #'PASSWORD': 'placybabe',
+        #'HOST': 'yatish13.mysql.pythonanywhere-services.com',
 
     }
 }
